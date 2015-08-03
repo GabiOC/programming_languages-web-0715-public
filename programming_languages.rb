94dd35f8cee8a6f29a03c23e73e7ebe2b7bc47fd
GabiOC <oconnor.gabriele@gmail.com>
2015-08-03 15:18:26 -0400

    all done, phew

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+
+  languages.each do |paradigm, all_languages|
+  	all_languages.each do |language_name, attribute|
+  		# binding.pry
+	  	if new_hash[language_name] != nil
+	  		new_hash[language_name][:style] << paradigm
+	  	else
+	  		new_hash[language_name] = { type: attribute[:type], style: [paradigm]}
+  		end
+  	end
+  end
+  new_hash
 end
